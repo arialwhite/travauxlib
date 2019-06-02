fbfccc60177467bbebe03a97cd95d42b811c10cc
guillotel <guillotel.j@sfeir.com>
2019-06-02 21:40:01 +0200

    add /docs

---
+++ docs/precache-manifest.92b9ddbe42f0de8d33d627672aa7ebb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d109e0132d317836eaded117908d9117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "195609e8acf47e6e5862",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7d2f9ad18ca02ce65b7e",
+    "url": "/static/css/main.c2ad29ae.chunk.css"
+  },
+  {
+    "revision": "195609e8acf47e6e5862",
+    "url": "/static/js/2.bc13f504.chunk.js"
+  },
+  {
+    "revision": "7d2f9ad18ca02ce65b7e",
+    "url": "/static/js/main.1a02f2b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
